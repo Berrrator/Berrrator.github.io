14bc8bfa9e896fb9787fcac19eaa9875301ef1de
Vlad <bebraaboii@gmail.com>
2023-11-07 00:34:19 +0200

    Gelya LOX

--- manyi/src/App.js
+++ manyi/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bEBROID.
+          Англеина лошпед
         </p>
         <a
           className="App-link"
