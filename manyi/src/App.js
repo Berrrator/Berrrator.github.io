3c33ce878b3d23e1cea772376261e60cb6d5b398
Vlad <bebraaboii@gmail.com>
2023-11-07 01:05:10 +0200

    rainbow

--- manyi/src/App.js
+++ manyi/src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Англеина лошпед
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="rainbow rainbow_text_animated">
+        Ангелина ГеЙ
+      </div>
     </div>
   );
 }
